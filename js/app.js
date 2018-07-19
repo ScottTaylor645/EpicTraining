03d08a19f7e6dceaaa8aa2ae25908128d1bf5a8d
scott <staylor645@gmail.com>
2018-07-19 19:35:26 -0400

    add Hamburger menu to mobile and change styling to Desktop

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Click event used to execute my javascript function that enables my Hamburger menu when viewed in mobile
+$(document).ready(function() {
+    $('.menu-toggle').click(function() {
+        $('ul').toggleClass('hidden');
+        // alert('It worked!');
+    })
+})
